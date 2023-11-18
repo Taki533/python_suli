a449e9a62a00e4bc9f8ec1a968a87edc8b502a3d
taki <Taki@gmail.com>
2023-11-18 20:18:04 +0100

    szerepjáték vége

---
+++ szovegHun.py
@@ -0,0 +1,19 @@
+text = {"Megérkezel a kazamata bejáratához. Csikorogva kinyílik az ajtó és egy sötét, dohos folyosó fogad.":"Megérkezel a kazamata bejáratához. Csikorogva kinyílik az ajtó és egy sötét, dohos folyosó fogad.",
+        "belépsz és gyyújtasz egy fáklyát":"",
+        "besétálsz a sötétbe":"",
+        "Túl hideg van, halálra fagytál!":"",
+        "újrakezdés":"",
+        "kilépés":"",
+        "Gyújtasz egy fáklyát. Balra van egy nyitott ajtó, de a fény nem hatol be. Jobbra egy zárt, rozoga ajtó.":"",
+        "Jobb oldali ajtó":"",
+        "Bal oldali ajtó":"",
+        #Bal oldali ajtó
+        "Besétálsz a nyitott szobába, a fáklyád nem ér semmit, ezért nem látsz és belesétálsz egy medvecsapdába.":"",
+        #Jobb oldali ajtó
+        "Megpróbálod kinyitni az ajtó. Nincs zárva, ezért lassan csikorogva kinyílik. Egy kobold van a szobában.":"",
+        "harc! (kockadobás)":"",
+        "menekülés":"",
+        "Gyáván megfutamodtál. A játék véget ért!":"",
+        "Kevesebbet dobtál mint az ellenfél, ezért csúnyán megvert! Meghaltál!":"",
+        "Nagyobbat dobtál mint az ellenfél, ezért csúnyán megverted!":"",
+        "":""}
--- szöveghHUN.py
+++
@@ -1,9 +0,0 @@
-text={"":"Reggel felébredtem, Mit tegyek most?",
-      "fogmosás":"fogmosás",
-      "Reggeli":"reggeli",
-      "Öltözés":"fölöltözés",
-      "66-os parancs":"66-os parancs",
-      "Elmegyek fogat mosni. Sikálom rendesen ahogy kell!":"Elmegyek fogat mosni. Sikálom rendesen ahogy kell!",
-      "Kellene valamit enni! Anya csinált valamit? Nézzük meg!":"Kellene valamit enni! Anya csinált valamit? Nézzük meg!",
-      "Kisséhűvösvan, Kellene valami ruha! \nFelveszek egy nadrágot, meg egy pólót":"Kisséhűvösvan, Kellene valami ruha! \nFelveszek egy nadrágot, meg egy pólót",
-      "végemindennek":"végemindennek",}
