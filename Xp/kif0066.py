614d378522e187a2e1b95b1b42d09ba33c6f80ff
taki <Taki@gmail.com>
2023-12-12 21:09:42 +0100

    xp

---
+++ Xp/kif0066.py
@@ -0,0 +1,3 @@
+print("Szerző: Takács Bence")
+szamitas=(((((43*2)+(77-23)))/(27.5*8))*((97*2)/50))
+print("A számítás eredmény: ",szamitas)
--- Xp/kif0071.py
+++
@@ -1,2 +0,0 @@
-szamitas=((12.3553*2)/32.747)+12.387
-print("a számítás eredmény: ",szamitas)
