e40bd9aa9935d4fade56c1c3552ce4ba513f5553
taki <Taki@gmail.com>
2023-12-13 12:31:26 +0100

    xp

---
+++ Xp/osztaly.py
@@ -0,0 +1,2 @@
+print("Gyártó: Takács Bence")
+print("\"Tér Erzsébet\"\t  \"Miskolc\"\n\"Nagy József\"\t \"Szolnok\"\n\"Tél Gábor\"\t\"Miskolc\"\n\"Rongy Elek\"\t \"Szeged\"\n\"Fer Tamás\"\t\"Miskolc\"")
